9d6b0422930e35386d7e603ff5b96711ded93ced
Adam Nejdl <nejdlad1@fel.cvut.cz>
2025-12-11 17:02:43 +0100

    Composed actionPerformed in ClearSelectionAction

--- jhotdraw-actions/src/main/java/org/jhotdraw/action/edit/ClearSelectionAction.java
+++ jhotdraw-actions/src/main/java/org/jhotdraw/action/edit/ClearSelectionAction.java
@@ -72,21 +72,33 @@ public class ClearSelectionAction extends AbstractSelectionAction {
 
     @Override
     public void actionPerformed(ActionEvent evt) {
-        JComponent c = target;
-        if (c == null && (KeyboardFocusManager.getCurrentKeyboardFocusManager().
-                getPermanentFocusOwner() instanceof JComponent)) {
-            c = (JComponent) KeyboardFocusManager.getCurrentKeyboardFocusManager().
-                    getPermanentFocusOwner();
+        JComponent currentTarget = findTargetComponent();
+
+        if (currentTarget != null && currentTarget.isEnabled()) {
+            performAction(currentTarget);
         }
-        if (c != null && c.isEnabled()) {
-            if (c instanceof EditableComponent) {
-                ((EditableComponent) c).clearSelection();
-            } else if (c instanceof JTextComponent) {
-                JTextComponent tc = ((JTextComponent) c);
-                tc.select(tc.getSelectionStart(), tc.getSelectionStart());
-            } else {
-                c.getToolkit().beep();
-            }
+    }
+
+    private JComponent findTargetComponent() {
+        JComponent currentTarget = target;
+        Component permanentFocusOwner = KeyboardFocusManager.getCurrentKeyboardFocusManager().
+                getPermanentFocusOwner();
+
+        if (currentTarget == null && (permanentFocusOwner instanceof JComponent)) {
+            currentTarget = (JComponent) permanentFocusOwner;
+        }
+
+        return currentTarget;
+    }
+
+    private void performAction(JComponent currentTarget) {
+        if (currentTarget instanceof EditableComponent) {
+            ((EditableComponent) currentTarget).clearSelection();
+        } else if (currentTarget instanceof JTextComponent) {
+            JTextComponent tc = ((JTextComponent) currentTarget;
+            tc.select(tc.getSelectionStart(), tc.getSelectionStart());
+        } else {
+            currentTarget.getToolkit().beep();
         }
     }
 
