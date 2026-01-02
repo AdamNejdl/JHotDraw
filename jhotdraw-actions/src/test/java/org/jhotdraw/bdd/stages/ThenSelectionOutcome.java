57b58f221d733fb4602ae5106b9271a801bb78a6
Adam Nejdl <nejdlik3@seznam.cz>
2026-01-02 21:21:17 +0100

    added bdd dependencies and created classes

---
+++ jhotdraw-actions/src/test/java/org/jhotdraw/bdd/stages/ThenSelectionOutcome.java
@@ -0,0 +1,7 @@
+package org.jhotdraw.bdd.stages;
+
+import com.tngtech.jgiven.Stage;
+
+public class ThenSelectionOutcome extends Stage<ThenSelectionOutcome> {
+
+}
---
+++ jhotdraw-actions/src/test/java/org/jhotdraw/bdd/stages/WhenSelectionAction.java
@@ -0,0 +1,6 @@
+package org.jhotdraw.bdd.stages;
+
+import com.tngtech.jgiven.Stage;
+
+public class WhenSelectionAction extends Stage<WhenSelectionAction> {
+}
