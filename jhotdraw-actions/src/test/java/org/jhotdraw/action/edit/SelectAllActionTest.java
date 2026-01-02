e277f7fd301181118891809506c611dc444fe49f
Adam Nejdl <nejdlik3@seznam.cz>
2026-01-02 13:42:29 +0100

    added SelectAllAction unit tests

--- jhotdraw-actions/src/test/java/org/jhotdraw/action/edit/SelectAllActionTest.java
+++ jhotdraw-actions/src/test/java/org/jhotdraw/action/edit/SelectAllActionTest.java
@@ -6,15 +6,42 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import java.awt.event.ActionEvent;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class SelectAllActionTest {
-    private SelectAllAction action;
-    private EditableComponent mockEditableComponent;
     private ActionEvent mockEvent;
 
+    @Before
+    public void setUp() {
+        mockEvent = mock(ActionEvent.class);
+    }
 
+    @Test
+    public void testActionPerformed_SelectsAll_OnJTextComponent() {
+        JTextComponent mockText = mock(JTextComponent.class);
+        when(mockText.isEnabled()).thenReturn(true);
+
+        SelectAllAction action = new SelectAllAction(mockText);
+
+        action.actionPerformed(mockEvent);
+
+        verify(mockText, times(1)).selectAll();
+    }
+
+    @Test
+    public void testActionPerformed_SelectsAll_OnEditableComponent() {
+        JComponent mockEditable = Mockito.mock(JComponent.class,
+                withSettings().extraInterfaces(EditableComponent.class));
+        when(mockEditable.isEnabled()).thenReturn(true);
+
+        SelectAllAction action = new SelectAllAction(mockEditable);
+
+        action.actionPerformed(mockEvent);
+
+        verify((EditableComponent) mockEditable, times(1)).selectAll();
+    }
 }
