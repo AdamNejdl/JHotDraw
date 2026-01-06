6bbbe2e7d68715b1471b13e138a06af1d75f5abf
Jannatul Fardous <jannatulfardous5309@gmail.com>
2026-01-06 22:34:48 +0100

    Add unit test for RectangleFigure bounds behavior

---
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/figure/RectangleFigureTest.java
@@ -0,0 +1,30 @@
+package org.jhotdraw.draw.figure;
+
+import org.testng.annotations.Test;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+import static org.testng.Assert.*;
+
+public class RectangleFigureTest {
+
+    @Test
+    public void testRectangleBoundsAfterSetBounds() {
+        // Arrange
+        RectangleFigure rectangle = new RectangleFigure();
+
+        Point2D.Double start = new Point2D.Double(10, 20);
+        Point2D.Double end   = new Point2D.Double(110, 70);
+
+        // Act
+        rectangle.setBounds(start, end);
+
+        // Assert
+        Rectangle2D.Double bounds = rectangle.getBounds();
+
+        assertEquals(bounds.getX(), 10.0, 0.001);
+        assertEquals(bounds.getY(), 20.0, 0.001);
+        assertEquals(bounds.getWidth(), 100.0, 0.001);
+        assertEquals(bounds.getHeight(), 50.0, 0.001);
+    }
+}
